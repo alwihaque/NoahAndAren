8c3774feb69b7af6042d1d54c003761816406e7e
alwihaque <alwihaque@icloud.com>
2021-07-25 21:54:48 +0100

    Setting up for image upload

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ import routerHandler from './Init/routes.js';
 const MONGO_URI = config.get('connection-config.MONGO_URI');
 const PORT = config.get('connection-config.PORT');
 const app = express();
+let storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname+ '-'+ Date.now());
+    }
+});
+
 routerHandler(app);
 dbConnectionHandler(MONGO_URI);
 
